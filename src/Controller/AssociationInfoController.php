9223404d7153ef7d25ce3ec47e3b82d95737bf4d
japhet <japhet-lys-goma.kaya@epitech.eu>
2024-05-22 14:15:49 +0200

    redirection vers la liste des associations

--- src/Controller/AssociationInfoController.php
+++ src/Controller/AssociationInfoController.php
@@ -36,12 +36,44 @@ class AssociationInfoController extends AbstractController
             $entityManager->flush();
 
             // Redirection vers une autre page après le succès de la soumission du formulaire
-            return $this->redirectToRoute('app_accueil');
+            return $this->redirectToRoute('association_list');
         }
 
         // Affichage de la page avec le formulaire
         return $this->render('association_info/index.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/association/{id}', name: 'association_show', methods: ['GET'])]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        // Convertit l'ID en un entier supplémentaire par mesure de précaution
+        $associationId = (int) $id;
+    
+        // Récupère l'entité Association correspondant à l'ID donné
+        $association = $entityManager->getRepository(Association::class)->find($id);
+    
+        // Vérifie si aucune association correspondante n'est trouvée
+        if (!$association) {
+            // Lance une exception NotFoundHttpException avec un message approprié
+            throw $this->createNotFoundException('The association does not exist');
+        }
+    
+        // Si une association est trouvée, transmet l'objet association au template
+        return $this->render('association_info/show.html.twig', [
+            'association' => $association,
+        ]);
+    }
+    
+    #[Route('/associations', name: 'association_list', methods: ['GET'])]
+    public function list(EntityManagerInterface $entityManager): Response
+    {
+        // Récupère toutes les associations à partir de la base de données
+        $associations = $entityManager->getRepository(Association::class)->findAll();
+    
+        // Transmet la liste des associations au template
+        return $this->render('association_info/list.html.twig', [
+            'associations' => $associations,
         ]);
     }
 }
